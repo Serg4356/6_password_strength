72c883b280cd9c02dddca276db0717e8f47067e7
Serg4356 <asdfg111000@yandex.ru>
2017-12-22 14:35:24 +0500

    More simplification for get_password_strength"

--- password_strength.py
+++ password_strength.py
@@ -4,20 +4,19 @@ import sys
 
 def get_password_strength(password):
     password_strength = 0
+    rules = [r'\d', r'\W', r'\d[.-]\d{2}[.-]\d', r'\d{11}']
+    for rule in rules[:1]:
+        if re.search(rule, password):
+            password_strength += 2
     if not (password.isupper() | password.islower()):
-        password_strength += 2
-    if re.search(r'\d', password):
-        password_strength += 2
-    if re.search(r'\W', password):
         password_strength += 2
     password_blacklist = ['One', 'Two', 'Three']
     for prohibited_word in password_blacklist:
         if re.search(prohibited_word, password):
             return 'Password is incorrect. Please, do not use prohibited words'
-    if re.search(r'\d[.-]\d{2}[.-]\d', password):
-        return 'Do not use date format in password'
-    if re.search(r'\d{11}', password):
-        return 'Do not use phone number in password'
+    for rule in rules[2:]:
+        if re.search(rule, password):
+            return 'Password is incorrect'
     return password_strength
 
 
