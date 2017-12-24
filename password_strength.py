959717181e3144e628877be3d912241d2dca7395
Serg4356 <asdfg111000@yandex.ru>
2017-12-24 14:41:50 +0500

    Add password_blacklist.txt file containing prohibited words, simplify get_password_strength by adding two more functions with separated functionality

--- password_strength.py
+++ password_strength.py
@@ -1,24 +1,32 @@
 import re
 import sys
+import getpass
 
+def get_password_balcklist(file_name):
+    with open(file_name, 'r', encoding='utf-8') as raw_file:
+        return(raw_file.read())
 
-def get_password_strength(password):
+def is_password_in_blacklist(password_blacklist,password):
+    for prohibited_word in password_blacklist:
+        if re.search(prohibited_word, password):
+            return True
+        else:
+            return False
+
+def get_password_strength(password, password_blacklist):
     password_strength = 0
-    rules = [r'\d', r'\W', r'\d[.-]\d{2}[.-]\d', r'\d{11}']
-    for rule in rules[:1]:
-        if re.search(rule, password):
+    patterns = [r'\d', r'\W', r'(?!.*\d{2,4}[.-]\d{2}[.-]\d{2,4})', r'\d{11}']
+    for pattern in patterns:
+        if re.search(pattern, password):
             password_strength += 2
     if not (password.isupper() | password.islower()):
         password_strength += 2
-    password_blacklist = ['One', 'Two', 'Three']
-    for prohibited_word in password_blacklist:
-        if re.search(prohibited_word, password):
-            return 'Password is incorrect. Please, do not use prohibited words'
-    for rule in rules[2:]:
-        if re.search(rule, password):
-            return 'Password is incorrect'
+    if not is_password_in_blacklist(password_blacklist,password): password_strength += 2
+
     return password_strength
 
 
 if __name__ == '__main__':
-    print(get_password_strength(sys.argv[1]))
+    password_blacklist = get_password_balcklist('password_blacklist.txt')
+    password = getpass.getpass()
+    print('Password strength is: ', get_password_strength(password,password_blacklist))
