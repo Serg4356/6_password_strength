2fd81b7afd20a961e1459e1d341b7df24bca1f94
Serg4356 <asdfg111000@yandex.ru>
2017-12-26 12:57:15 +0500

    Add comment to regular expression. Add Quickstart chapter to Readme.md

--- README.md
+++ README.md
@@ -6,11 +6,36 @@ This programm checks the password by list of demands, such as:
 2. inclusion of one or more numerical digits
 3. inclusion of special characters, such as @, #, $
 4. prohibition of words found in a password blacklist
-5. prohibition of words found in the user's personal information
-6. prohibition of use of company name or an abbreviation
-7. prohibition of passwords that match the format of calendar dates, license plate numbers, telephone numbers, or other common numbers
+5. prohibition of passwords that match the format of calendar dates and telephone numbers.
 
-And also returns a grade of password from 1 to 10, where 10 is the most secure.
+And also returns a grade of password from 1 to 10, where 10 is the most secure. In case when prohibition rule was violated programm returns: 
+
+``` bash
+
+'Password strength is 0' 
+
+```
+
+# Quickstart
+
+Example of script launch on Windows, Python 3.5:
+
+``` bash
+
+$ python password_strength.py
+
+```
+
+After that programm asks the user to enter password. Example of programm output:
+
+``` bash
+
+Password:
+# password input is shielded so there is no letter appeares
+Password strength is: 0
+
+
+```
 
 # Project Goals
 
--- password_strength.py
+++ password_strength.py
@@ -1,36 +1,86 @@
 import re
-import sys
 import getpass
 
 
-def get_password_balcklist(file_name):
+def load_file(file_name):
     with open(file_name, 'r', encoding='utf-8') as raw_file:
         return raw_file.read()
 
 
-def is_password_in_blacklist(password_blacklist, password):
-    for prohibited_word in password_blacklist:
-        if re.search(prohibited_word, password):
-            return True
-        else:
-            return False
+def is_word_in_blacklist(blacklist, word):
+    if word.lower() in blacklist:
+        return True
 
 
-def get_password_strength(password, password_blacklist):
+def has_numbers(word):
+    if re.search(r'\d', word):
+        return True
+
+
+def has_symbols(word):
+    if re.search(r'\W', word):
+        return True
+
+
+def is_min_length(word, min_length):
+    if len(word) >= min_length:
+        return True
+
+
+def has_uppercase(word):
+    if re.search(r'[A-Z]', word):
+        return True
+
+
+def has_lowercase(word):
+    if re.search(r'[a-z]', word):
+        return True
+
+
+def is_not_date(word):
+    if not re.search(r'\d{2,4}[.-]\d{2}[.-]\d{2,4}(?#searches for date in the string)', word):
+        return True
+
+
+def is_not_phone_number(word):
+    if not re.search(r'[7-8]\d{10}', word):
+        return True
+
+
+def is_not_empty(word):
+    if word != '':
+        return True
+
+
+def get_password_strength(password, password_blacklist_file_name):
     password_strength = 0
-    patterns = [r'\d', r'\W', r'(?!.*\d{2,4}[.-]\d{2}[.-]\d{2,4})', r'\d{11}']
-    for pattern in patterns:
-        if re.search(pattern, password):
-            password_strength += 2
-    if not (password.isupper() | password.islower()):
-        password_strength += 2
-    if not is_password_in_blacklist(password_blacklist,password):
-        password_strength += 2
-
+    if is_not_empty(password):
+        password_strength += 1
+    if is_min_length(password, 10):
+        password_strength += 1
+    if is_not_date(password):
+        password_strength += 1
+    else:
+        return 0
+    if is_not_phone_number(password):
+        password_strength += 1
+    else:
+        return 0
+    if has_lowercase(password):
+        password_strength += 1
+    if has_uppercase(password):
+        password_strength += 1
+    if has_numbers(password):
+        password_strength += 1
+    if has_symbols(password):
+        password_strength += 1
+    if is_word_in_blacklist(load_file(password_blacklist_file_name), password):
+        password_strength += 1
+    else:
+        return 0
     return password_strength
 
 
 if __name__ == '__main__':
-    password_blacklist = get_password_balcklist('password_blacklist.txt')
     password = getpass.getpass()
-    print('Password strength is: ', get_password_strength(password,password_blacklist))
+    print('Password strength is: ', get_password_strength(password, 'password_blacklist.txt'))
